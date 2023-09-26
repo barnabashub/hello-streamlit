777cad7c32c34922a20a8d5970c1ee0c5b165c8f
barnabashub <66264092+barnabashub@users.noreply.github.com>
2023-09-26 16:56:56 +0000

    year easy slider

--- Hello.py
+++ Hello.py
@@ -1,51 +1,71 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas as pd
+from streamlit_vizzu import VizzuChart, Config, Data
 
 LOGGER = get_logger(__name__)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+# Create a VizzuChart object with the default height and width
+chart = VizzuChart()
+
+# Generate some data and add it to the chart
+df = pd.DataFrame({"a": ["x", "y", "z"], "b": [1, 2, 3]})
+data = Data()
+data.add_data_frame(df)
+chart.animate(data)
+
+# Add some configuration to tell Vizzu how to display the data
+chart.animate(Config({"x": "a", "y": "b", "title": "Look at my plot!"}))
+#chart.animate(Config({"x": "b", "y": "a", "title": "Look at my plot!"}))
+
+if st.checkbox("Swap"):
+    chart.animate(Config({"y": "a", "x": "b", "title": "Swapped!"}))
+
+if st.toggle("Order"): 
+    chart.animate(
+    Config(
+        {
+            "sort": "none",
+            "reverse": True,
+        }
+    )
+    )    
+else:
+    chart.animate(
+    Config(
+        {
+            "sort": "byValue",
+            "reverse": False,
+        }
+    )
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+if st.button("Aggregate"):
+    chart.animate(
+    Config(
+        {
+            "channels": {
+                "x": {"set": None},
+            }
+        }
+        )
+    )
+    chart.animate(
+    Config(
+        {
+            "channels": {
+                "x": "a", "y": "b"
+            }
+        }
+        )
     )
 
+if "marker" in data:
+    st.write("value of clicked bar:", data["marker"]["values"]["b"])
 
-if __name__ == "__main__":
-    run()
+
+# Show the chart in the app!
+#chart.show()
+data = chart.show()
+st.write(data)
--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,35 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
+import pandas as pd
 import streamlit as st
-from streamlit.hello.utils import show_code
+from streamlit_vizzu import Config, Data, Style, VizzuChart
+#import streamlit_elements as elements
 
+df = pd.read_csv("sales_data_sample.csv", sep=",", encoding='Latin-1')
+st.table(df[0:4])
 
-def animation_demo() -> None:
+data = Data()
+data.add_data_frame(df)
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+#Make a slider
+yearValue = st.slider("Pick a year", min_value=2002, max_value=2005, value=2003)
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+#Make checkbox for years
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
+#Make checkbox for quaerter year
 
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
-
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
-
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
-
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
-
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
-
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
+chart2 = VizzuChart(height=380)
+chart2.animate(data)
+chart2.feature("tooltip", True)
+yearFilter = "record['YEAR_ID'] == '{year}'".format(year = yearValue)
+chart2.animate(
+    Data.filter(yearFilter),
+    Config(
+        {
+            "channels": {
+                "y": {"set": ["STATUS"]},
+                "x": {"set": ["ORDERNUMBER"]},
+            }
+        }
+    )
 )
 
-animation_demo()
-
-show_code(animation_demo)
+chart2.show()
