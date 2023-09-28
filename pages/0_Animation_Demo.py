fc00d163074a68a5d7699333f85dd9b6c8fcefa6
barnabashub <66264092+barnabashub@users.noreply.github.com>
2023-09-28 14:28:43 +0000

    format multiselect element on the place

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,5 +1,7 @@
+from typing import List
 import pandas as pd
 import streamlit as st
+import numpy as np
 from streamlit_vizzu import Config, Data, Style, VizzuChart
 #import streamlit_elements as elements
 
@@ -10,18 +12,29 @@ data = Data()
 data.add_data_frame(df)
 
 #Make a slider
-yearValue = st.slider("Pick a year", min_value=2002, max_value=2005, value=2003)
+#yearValue = st.slider("Pick a year", min_value=2002, max_value=2005, value=2003)
+year1, year2 = st.select_slider(
+    "Time range", options=map(str, np.arange(2002, 2006)), value=("2004", "2005")
+)
+yearFilter = "record['YEAR_ID'] >= '{yearMin}' && record['YEAR_ID'] <= '{yearMax}'".format(yearMin = year1, yearMax = year2)
 
-#Make checkbox for years
-
-#Make checkbox for quaerter year
+# Make select for productline
+allFormats = df["PRODUCTLINE"].unique()
+defaultFormats = []
+items: List[str] = st.multiselect(
+    "Product line", allFormats, defaultFormats, key="multiselect"
+)
+filter_format = (
+    "(" + " || ".join([f"record['Format'] == '{item}'" for item in items]) + ")"
+)
+formatFilter = "record['PRODUCTLINE'] in items"
 
 chart2 = VizzuChart(height=380)
 chart2.animate(data)
 chart2.feature("tooltip", True)
-yearFilter = "record['YEAR_ID'] == '{year}'".format(year = yearValue)
+filters = "{} and {}".format(yearFilter, formatFilter)
 chart2.animate(
-    Data.filter(yearFilter),
+    Data.filter(filters),
     Config(
         {
             "channels": {
